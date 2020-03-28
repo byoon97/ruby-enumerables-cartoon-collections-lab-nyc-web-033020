8a9caeec2dc41b6a7e39e836000a658d06dc78b7
Brandon Yoon <byoon5397@gmail.com>
2020-03-28 21:54:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,12 @@ def summon_captain_planet(array)
   array.map { |element| element.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |word|
+    if word.length > 4
+      return true
+    end
+  return false
 end
 
 def find_the_cheese# code an argument here
