c2744c710d97016fe2495224d449c7c260df65ca
Brandon Yoon <byoon5397@gmail.com>
2020-03-28 21:58:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,11 @@ def long_planeteer_calls(array)
    end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |food|
+    if food.include?(cheese_types)
+      return food
+    end
+  end
 end
